087c4bccd7d707ad00f1dc3194a6bac50605ac8b
erfan-web <erfan.dev85@gmail.com>
2026-04-26 20:30:27 +0330

    Reviewing and adding a guide to the analog clock

--- analog-clock/script.js
+++ analog-clock/script.js
@@ -1,16 +1,22 @@
+const h = document.getElementById("hour");
+const m = document.getElementById("minute");
+const s = document.getElementById("second");
+
 function updateAnalogClock() {
   const now = new Date();
   const hours = now.getHours() % 12;
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  const hourDeg = (hours + minutes/60) * 30; // 360/12 = 30
-  const minuteDeg = (minutes + seconds/60) * 6; // 360/60 = 6
+  const hourDeg = (hours + minutes / 60) * 30;
+  const minuteDeg = (minutes + seconds / 60) * 6;
   const secondDeg = seconds * 6;
+  //   const minuteDeg = minutes * 6;
+  //   const hourDeg = hours * 30;
 
-  document.getElementById("hour").style.transform = `rotateZ(${hourDeg}deg)`;
-  document.getElementById("minute").style.transform = `rotateZ(${minuteDeg}deg)`;
-  document.getElementById("second").style.transform = `rotateZ(${secondDeg}deg)`;
+  s.style.transform = `rotateZ(${secondDeg}deg)`;
+  m.style.transform = `rotateZ(${minuteDeg}deg)`;
+  h.style.transform = `rotateZ(${hourDeg}deg)`;
 }
 
 setInterval(updateAnalogClock, 1000);
